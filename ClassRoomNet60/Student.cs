adfd9ae1ea5e7a2b3516cb980038e82157b28737
Christian Lund Nielsen <chn002@edu.zealand.dk>
2024-08-27 13:40:03 +0200

    Tilføjet Student class

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+       
+        //Properties
+        public string Name { get; private set; }
+        public int BirthMonth { get; private set; }
+        public int Birthday { get; private set; }
+
+
+
+        //Konstruktør
+        public Student(string name, int birthMonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            Birthday = birthday;
+        }
+
+
+
+    }
+}
